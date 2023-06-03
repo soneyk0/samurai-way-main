95b5fefe5c9e0952999c85194226bf3412d4ef59
Yana <yana.prokopenko24@mai.ru>
2023-06-03 13:55:04 +0300

    components

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header =()=>{
+    return(
+        <header className='header'>
+            <img src="https://api.freelogodesign.org/assets/thumb/logo/5320_400.png" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Profile = () => {
+    return(<div className='content'>
+        <div><img
+            src="https://www.mirf.ru/wp-content/uploads/2022/02/1522977132_128_0_7374_4076_1920x0_80_0_0_04ec5fd1abe7f78c95e028c368cc1e7c.jpg"
+            alt=""/></div>
+        <div>ava+descrip</div>
+        <div>my post
+            <div>new post</div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    </div>
+)};
+
+export default Profile;
