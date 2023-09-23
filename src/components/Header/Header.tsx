2015d83824e8811b7a6707ec041e9bd2ac496391
Yana <yana.prokopenko24@mai.ru>
2023-09-23 16:06:23 +0300

    login css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,8 @@ const Header = (props: HeaderTypePage) => {
             <img src='https://nettox.net/upload/CAllcorp3/312/hf0t8t1e29hr26hkqxmscb57emme8k46.svg' alt=""/>
 
             <div className={s.loginBlock}>
-                {props.isAuth ?  props.login
+                {props.isAuth ? <span className={s.login}>{props.login}</span>
+
                     : <NavLink to={'/login'} activeClassName={s.activeLink}>Login</NavLink>}
             </div>
         </header>
